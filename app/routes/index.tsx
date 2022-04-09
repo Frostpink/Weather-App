83bbb4205e6089506e2bf7034b88f6301d48ce74
Frostpink <99618915+Frostpink@users.noreply.github.com>
2022-04-09 03:23:27 -0400

    feat:  mantine theme

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,25 @@
+import { Box, Container, Grid, TextInput } from "@mantine/core"
+import { useState } from "react"
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <Container size='xs'>
+            <TextInput
+            variant='filled'
+  placeholder="Your name"
+  label="Full name"
+  radius="md"
+  size="md"
+  required
+/>
+            <TextInput
+            variant='filled'
+  placeholder="exampleuser"
+  label="Username"
+  radius="md"
+  size="md"
+  required
+/>
+        </Container>
+    )
 }
