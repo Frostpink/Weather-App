1d1e92bb01c68a73e39bcdf7da5648f2d4a3c2b4
Pink <missfussy24@outlook.com>
2022-04-13 18:26:36 -0400

    feat: reformat with prettier

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,13 @@
 /**
+ * @format
  * @type {import('@remix-run/dev/config').AppConfig}
  */
+
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002,
-  ignoredRouteFiles: [".*"]
-};
+    appDirectory: 'app',
+    assetsBuildDirectory: 'public/build',
+    publicPath: '/build/',
+    serverBuildDirectory: 'build',
+    devServerPort: 8002,
+    ignoredRouteFiles: ['.*'],
+}
