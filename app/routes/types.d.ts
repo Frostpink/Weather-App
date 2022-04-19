8bce0816297e5f0c586ac4d7255c5418265808d2
Pink <missfussy24@outlook.com>
2022-04-19 15:43:41 -0400

    feat: functional get location & current weather (with additional wip)
    This commit adds a working search for location using the top search bar
    and on click button.
    It also adds types for daily weather and hourly weather.
    There's also some wip for using a service rather than keeping everything
    in the main tsx file.

---
+++ app/routes/types.d.ts
@@ -0,0 +1,73 @@
+/** @format */
+
+export type Location = {
+    Key: string
+    Type: string
+    LocalizedName: string
+    Country: { ID: string; LocalizedName: string }
+    AdministrativeArea: { ID: string; LocalizedName: string }
+}
+
+// export type WeatherDay = {
+//     Date: string
+//     EpochDate: number
+//     Temperature: {
+//         Minimum: { Value: number; Unit: string; UnitType: number }
+//         Maximum: { Value: number; Unit: string; UnitType: number }
+//     }
+//     Day: {
+//         Icon: number
+//         IconPhrase: string
+//         HasPrecipitation: boolean
+//         PrecipitationType: string
+//         PrecipitationIntensity: string
+//         PrecipitationProbability: number
+//     }
+//     Night: {
+//         Icon: number
+//         IconPhrase: string
+//         HasPrecipitation: boolean
+//         PrecipitationType: string
+//         PrecipitationIntensity: string
+//         PrecipitationProbability: number
+//     }
+// }
+
+export type WeatherHour = {
+    DateTime: string
+    EpochDateTime: number
+    WeatherIcon: number
+    IconPhrase: string
+    HasPrecipitation: boolean
+    IsDaylight: boolean
+    Temperature: {
+        Value: number
+        Unit: string
+        UnitType: number
+    }
+    PrecipitationProbability: number
+    MobileLink: string
+    Link: string
+}
+
+export type WeatherCurrent = {
+    LocalObservationDateTime: string
+    EpochTime?: number
+    WeatherText?: string
+    WeatherIcon: number
+    HasPrecipitation?: boolean
+    PrecipitationType?: string
+    IsDayTime?: boolean
+    Temperature: {
+        Metric: Temperature
+        Imperial?: Temperature
+    }
+    MobileLink?: string
+    Link?: string
+}
+
+type Temperature = {
+    Value: number
+    Unit: string
+    UnitType: number
+}
