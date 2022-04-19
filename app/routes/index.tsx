fd3cf8c5ad2c025a0b80a9cd06b5bc9147f675e6
Pink <missfussy24@outlook.com>
2022-04-19 15:43:41 -0400

    feat: functional get location & current weather (with additional wip)
    This commit adds a working search for location using the top search bar
    and on click button.
    It also adds types for daily weather and hourly weather.
    There's also some wip for using a service rather than keeping everything
    in the main tsx file.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,11 @@
 /** @format */
+import axios, { AxiosResponse } from 'axios'
+import { useState } from 'react'
+
+// import WeatherService from '../services/weather.service'
+// import http from '../http-common'
+
+import { WeatherCurrent, Location } from './types'
 
 // primary background color: gray-700
 // primary text color: gray-200
@@ -16,6 +23,55 @@ function WeatherCard({ text, temperature }: { text: string; temperature: number 
 }
 
 export default function Index() {
+    type GetLocationProps = {
+        method: string
+        url: string
+        params: { apikey: string; q: string; language: string }
+    }
+
+    const [currentWeather, setCurrentWeather] = useState<WeatherCurrent>({
+        LocalObservationDateTime: '2022-04-18T00:00:00-04:00',
+        WeatherIcon: 34,
+        Temperature: {
+            Metric: {
+                Value: 1.2,
+                Unit: 'C',
+                UnitType: 17,
+            },
+        },
+    })
+
+    const [location, setLocation] = useState<Location | null>(null)
+
+    const [locationInput, setLocationInput] = useState<string>('')
+
+    const onClickHandler = async () => {
+        // TODO: change this function to use the weather service
+        // const { locations, errors } = await WeatherService.getLocationKey('ottawa')
+
+        const options = {
+            method: 'GET',
+            url: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+            params: {
+                apikey: 'zFCZuhthsAW0V2YMisJwFqGGB1KLoAva',
+                q: locationInput,
+                language: 'en-CA',
+            },
+        } as GetLocationProps
+
+        axios
+            .get<GetLocationProps, AxiosResponse<Location[]>>(options.url, {
+                params: options.params,
+            })
+            .then(response => {
+                if (response.data.length <= 0) throw new Error('No locations found')
+                setLocation(response.data[0])
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
     return (
         <>
             <div className='flex h-screen w-screen flex-col bg-gray-700'>
@@ -24,21 +80,38 @@ export default function Index() {
                         <p className=''>S</p>
                         <input
                             placeholder='Search city'
+                            value={locationInput}
+                            onChange={e => setLocationInput(e.target.value)}
                             className='my-2 w-full border-none bg-transparent text-sm outline-none focus:outline-none'
                         />
+                        <button className='' onClick={onClickHandler}>
+                            *
+                        </button>
                     </div>
                 </div>
+
                 <div className='flex flex-col items-center text-gray-200'>
                     <div className='flex flex-row items-center'>
                         <p>ICON</p>
                         <div className='text-center'>
                             <p className='text-3xl font-bold'>Today</p>
-                            <p className='text-xs font-semibold'>Wed, 23 Feb</p>
+                            <p className='text-xs font-semibold'>
+                                {new Date(currentWeather.LocalObservationDateTime)
+                                    .toDateString()
+                                    .replace(/(\w+)\s(\w+)\s(\w+)\s(\w+)/, '$1, $2 $3')}
+                            </p>
                         </div>
                     </div>
-                    <p className='text-6xl font-semibold'>-32</p>
-                    <p className='text-xs font-semibold'>Ottawa, Ontario</p>
+                    <p className='text-6xl font-semibold'>
+                        {currentWeather.Temperature.Metric.Value}
+                    </p>
+                    <p className='text-xs font-semibold'>
+                        {location
+                            ? `${location.LocalizedName}, ${location.AdministrativeArea.LocalizedName}`
+                            : 'no location'}
+                    </p>
                 </div>
+
                 <div>
                     <div className='flex flex-col  gap-y-6'>
                         <div className='mx-auto flex gap-x-3'>
@@ -48,6 +121,7 @@ export default function Index() {
                                 </div>
                             ))}
                         </div>
+
                         <div className='mx-auto flex gap-x-3'>
                             {[...Array(4)].map((_, i) => (
                                 <div className='' key={i}>
