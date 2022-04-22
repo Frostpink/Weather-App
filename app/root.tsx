fa173ae954d725f9e2eadd32ac21a46c19736aad
Frostpink <99618915+Frostpink@users.noreply.github.com>
2022-04-21 23:13:57 -0400

    fix: remove unsed brackets

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export default function App() {
                 <Outlet />
                 <ScrollRestoration />
                 <Scripts />
-                {<LiveReload />}
+                <LiveReload />
             </body>
         </html>
     )
