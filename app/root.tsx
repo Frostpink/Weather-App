da6c7a2e6c5ec46fa0dc97347b3e9f8b4ee8634c
Frostpink <99618915+Frostpink@users.noreply.github.com>
2022-04-21 22:11:31 -0400

    fix: server and client files now use the new packages

--- app/root.tsx
+++ app/root.tsx
@@ -1,21 +1,19 @@
 /** @format */
 
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/node'
 import type { MetaFunction, LinksFunction } from 'remix'
 
 import styles from './styles/app.css'
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
 export const meta: MetaFunction = () => {
-    return { title: 'New Remix App' }
+    return { title: 'New Remix App', charset: "utf-8", viweport: "width=device-width,inicial-scale=1" }
 }
 
 export default function App() {
     return (
         <html lang='en'>
             <head>
-                <meta charSet='utf-8' />
-                <meta name='viewport' content='width=device-width,initial-scale=1' />
                 <Meta />
                 <Links />
             </head>
@@ -23,7 +21,7 @@ export default function App() {
                 <Outlet />
                 <ScrollRestoration />
                 <Scripts />
-                {process.env.NODE_ENV === 'development' && <LiveReload />}
+                {<LiveReload />}
             </body>
         </html>
     )
--- remix.config.js
+++ remix.config.js
@@ -4,12 +4,7 @@
  */
 
 module.exports = {
-    appDirectory: 'app',
-    assetsBuildDirectory: 'public/build',
-    publicPath: '/build/',
-    serverBuildDirectory: 'build',
-    devServerPort: 8002,
-    ignoredRouteFiles: ['.*'],
-    serverBuildPath: "build/index.js",
     serverBuildTarget: "vercel",
+    server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+    ignoredRouteFiles: ["**/*"],
 }
